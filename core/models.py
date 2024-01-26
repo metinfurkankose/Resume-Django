4358361cb8149367a1c0e291df73869c20ddb38c
Metin <metinfurkankose@hotmail.com>
2024-01-26 19:25:19 +0300

    feat: create image setting model

--- core/models.py
+++ core/models.py
@@ -41,3 +41,44 @@ class GeneralSetting(models.Model):
         verbose_name = 'General Setting'
         verbose_name_plural = 'General Settings'
         ordering = ('name','description')
+
+class ImageSetting(models.Model):
+    name = models.CharField(
+        default='',
+        max_length=255,
+        blank=True,
+        verbose_name='Name',
+        help_text='This is veriable of the setting.',
+    )
+    description = models.CharField(
+        default='',
+        max_length=255,
+        blank=True,
+        verbose_name='Description',
+        help_text='',
+    )
+    file = models.ImageField(
+        default='',
+        verbose_name='Image',
+        help_text='',
+        blank=True,
+        upload_to='images/',
+    )
+    updated_date = models.DateTimeField(
+        blank=True,
+        auto_now=True,
+        verbose_name='Updated Date',
+        help_text='',
+    )
+    created_date = models.DateTimeField(
+        blank=True,
+        auto_now_add=True,
+        verbose_name='Created Date',
+        help_text='',
+    )
+    def __str__(self):
+        return f'Image Setting: {self.name}'
+    class Meta:
+        verbose_name = 'Image Setting'
+        verbose_name_plural = 'Image Settings'
+        ordering = ('name','description')
